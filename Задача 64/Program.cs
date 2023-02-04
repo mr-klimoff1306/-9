902cb21b4c7e7f25f4c2b439ad41fec0af248697
klimoff <mr.klimoff1306@gmail.com>
2023-02-05 00:10:57 +0300

    Task 64 Solution

---
+++ Задача 64/Program.cs
@@ -0,0 +1,16 @@
+﻿ //Задайте значение N. Напишите программу,
+ // которая выведет все натуральные числа в промежутке от N до 1.
+
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+///Метод вывода натуральных чисел от N до 1:
+void PrintNumbers(int n)
+{
+    if (n == 0) return;
+    Console.Write("{0,4}", n);
+    PrintNumbers(n - 1);
+}
+
+PrintNumbers(number);
